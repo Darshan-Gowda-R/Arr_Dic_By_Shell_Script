52bf811c7e98c2dbe974903806411e71ee3cce2d
darshan <darshan.gowda.r.100@gmail.com>
2022-01-12 20:26:58 +0530

    the branch uc1

--- uc1.sh
+++ uc1.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -x
-read -p "Enter 3 numbers : " x y z
+read -p "Enter 3 numbers : " a b c
 
-echo $z $y $x
+echo $a $b $c
+declare -A  comp
+
+comp['a+b*c']=$((a+b*c))
+
+echo ${comp['a+b*c']}
